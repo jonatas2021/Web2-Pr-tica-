9d69e76368467af5905a4219844356f4b4aabb21
jonatas2021 <jonatascandido0@gmail.com>
2024-08-19 22:26:06 -0300

    atividade 2

---
+++ atividade2/biblioteca/database/migrations/2024_08_20_011648_create_books_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            // Definindo chaves estrangeiras
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
